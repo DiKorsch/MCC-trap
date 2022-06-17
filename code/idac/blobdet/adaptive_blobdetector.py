b50725f975ff707489cd0fd66cfce3cf94d5b699
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2022-06-17 22:56:23 +0200

    some updates in the code

--- code/idac/blobdet/adaptive_blobdetector.py
+++ code/idac/blobdet/adaptive_blobdetector.py
@@ -1,8 +1,11 @@
 import cv2
+import logging
 import numpy as np
 from idac.objectOfInterrest import ObjectOfInterrest
 from idac.blobdet.blob_detector import Blobdetector
 import time
+
+import matplotlib.pyplot as plt
 
 #Reference adaptive - https://docs.opencv.org/3.4/d7/d4d/tutorial_py_thresholding.html
 #Last accessed 17/12 - 2019
@@ -25,24 +28,33 @@ class AdaptiveBlobDetector(Blobdetector):
         gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         
         if self.usebackground: # Substract background image
-            gray = gray - self.background
+            h, w = gray.shape
+            bg = cv2.resize(self.background, (w, h)).astype(np.float32)
+            gray = 255 - np.abs(gray.astype(np.float32) - bg).astype(gray.dtype)
             
         gray = cv2.medianBlur(gray, self.medianblur)
+        # fig, (ax0, ax1, ax2) = plt.subplots(1,3)
+
+        # ax0.imshow(gray)
             
         time1 = time.time()
         gray = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                      cv2.THRESH_BINARY, self.adapsize, self.adapconst)
+        # ax1.imshow(gray)
         time2 = time.time()
-        print('Adaptive took {:.3f} ms'.format((time2 - time1) * 1000.0))
+        logging.debug('Adaptive took {:.3f} ms'.format((time2 - time1) * 1000.0))
         kernel = np.ones((self.kernel_adap, self.kernel_adap), np.uint8)
         gray_closed = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, kernel)
         original = cv2.morphologyEx(gray_closed, cv2.MORPH_OPEN, kernel)
         binary = original.copy()
+        # ax2.imshow(binary)
+
+        # plt.show()
         ooi = []
         startingid = startingid
         # Init counters for counting number of images
         count = 0
-        _, contours, _= cv2.findContours(255 - binary.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        *_, contours, _= cv2.findContours(255 - binary.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         for cnt in contours:
             x, y, w, h = cv2.boundingRect(cnt)
             crop_img = original[y:y + h, x:x + w]
